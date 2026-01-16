96b0d889ce74fa34a87257c673fad90916d97143
mishel2626 <mishelspaho@gmail.com>
2026-01-16 10:15:41 +0100

    ES6_basic

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,16 @@
+const today = "today";
+
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {  //true
+  const task = true;
+  const task2 = false;
+  }
+
+  console.log(today);
+
+  return [task, task2];
+}
+
