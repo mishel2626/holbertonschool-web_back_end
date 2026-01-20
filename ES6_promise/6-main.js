a2e1dbaf6fd5fc096a685053dcf14708540993a8
mishel2626 <mishelspaho@gmail.com>
2026-01-20 10:55:37 +0100

    commit

---
+++ ES6_promise/6-main.js
@@ -0,0 +1,7 @@
+import handleProfileSignup from './6-final-user.js';
+
+handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg').then((result) => {
+
+    console.log("result---", result);
+    return result
+})
