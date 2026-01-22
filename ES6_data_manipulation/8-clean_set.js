2b2fa4a6babd61762cc65febcd60a3bbd21617ed
mishel2626 <mishelspaho@gmail.com>
2026-01-22 12:19:08 +0100

    commit

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,15 @@
 export default function cleanSet(set, startString) {
-	const arr = [];
-	if (typeof startString === 'undefined' || startString === '' || typeof startString !== 'string') {
-		return '';
-	}
-	set.forEach((element) => {
-		if (typeof element !== 'undefined') {
-			if (element.startsWith(startString)) {
-				arr.push(element.split(startString)[1]);
-			}
-		}
-	});
-	return arr.join('-');
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  const result = [];
+
+  set.forEach((value) => {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
 }
